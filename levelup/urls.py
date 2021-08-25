4e41eeef426e0e5d21340b0bec5fd55c16d3e239
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-08-24 19:03:01 -0500

    Balanace restored, never use drop table again please

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,6 +17,7 @@ router.register(r'profile', ProfileView, 'profile')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include('levelupreports.urls')),
     # Requests to http://localhost:8000/register will be routed to the register_user function
     path('register', register_user),
     # Requests to http://localhost:8000/login will be routed to the login_user function
